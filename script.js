f2ebe86d0e2764e7504787b75a1b6e7c45f73210
Daniel Whitney <whitneyd@southwestern.edu>
2020-06-10 19:32:59 -0500

    added filter functionality

--- script.js
+++ script.js
@@ -39,18 +39,35 @@ async function showPosts() {
   });
 }
 
-//show loader, get more posts
+//toggle loader
 async function showLoading() {
   loading.classList.toggle("show");
 }
 
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+
+  const posts = document.querySelectorAll(".post");
+  posts.forEach((post) => {
+    const title = post.querySelector(".post-title").innerText.toUpperCase();
+    const body = post.querySelector(".post-body").innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = "flex";
+    } else {
+      post.style.display = "none";
+    }
+  });
+}
 showPosts();
 
 window.addEventListener("scroll", () => {
   const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-  console.log(scrollTop, scrollHeight, clientHeight);
+
   if (scrollTop + clientHeight >= scrollHeight - 5) {
     page++;
     showPosts();
   }
 });
+
+filter.addEventListener("input", filterPosts);
