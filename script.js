7787596ee5f62e128a29d8b63a959b334a25c5cd
Daniel Whitney <whitneyd@southwestern.edu>
2020-06-10 19:21:53 -0500

    add scroll event listener

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ const postsContainer = document.getElementById("post-container");
 const loading = document.querySelector(".loader");
 const filter = document.getElementById("filter");
 
-let limit = 3;
+let limit = 5;
 let page = 1;
-
+let data;
 // fetch post from api
 async function getPost() {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&page=${page}`
-  );
-
-  const data = await res.json();
-
+    `https://jsonplaceholder.typicode.com/posts?_limit=${limit}&_page=${page}`
+  ).then((response) => {
+    if (response.status == 200) {
+      showLoading();
+      data = response.json();
+    }
+  });
   return data;
 }
 
 // show posts in DOM
 async function showPosts() {
+  showLoading();
   const posts = await getPost();
+
   // posts will be an array, iterate through and add to dom
   posts.forEach((post) => {
     const postEl = document.createElement("div");
@@ -35,4 +39,18 @@ async function showPosts() {
   });
 }
 
+//show loader, get more posts
+async function showLoading() {
+  loading.classList.toggle("show");
+}
+
 showPosts();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  console.log(scrollTop, scrollHeight, clientHeight);
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    page++;
+    showPosts();
+  }
+});
